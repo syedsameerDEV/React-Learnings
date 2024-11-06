e7139c53bdb3c6f5bfca007deb8653c51c2584fb
Syed Sameer <syedsameer.dev20@gmail.com>
2024-11-06 10:20:27 +0530

    updated

---
+++ 06route/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import {Route, Router, Routes} from 'react-router-dom'
+function App() {
+  return (
+    <div>
+      <Router></Router>
+    </div>
+  );
+}
+
+export default App;
