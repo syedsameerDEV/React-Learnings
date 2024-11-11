34a7443910f1e0b5cdfa37ad97811f889ad1b2cd
Syed Sameer <syedsameer.dev20@gmail.com>
2024-11-11 11:59:55 +0530

    updated the usecontext

--- 07useContext/src/AddForm.jsx
+++ 07useContext/src/AddForm.jsx
@@ -1,18 +1,20 @@
 import React, { useContext, useState } from 'react'
 import { TodoContext} from './App'
 const AddForm = () => {
-    const data = useContext(TodoContext)
+    const {saveTodo,setSaveTodo} = useContext(TodoContext)
+console.log(saveTodo);
+
     const [addTodo,setAddTodo] = useState('')
     const handelTodo = (e)=>{
         setAddTodo(e.target.value)
     }
     const handelAdd = ()=>{
-      console.log(data.setSaveTodo);
-      
+      setSaveTodo([...saveTodo,addTodo])
+      setAddTodo('')
     }
   return (
     <div className='flex justify-center gap-2 m-10'>
-        <input type="text" placeholder='Add your Task..!' onChange={handelTodo} />
+        <input type="text" placeholder='Add your Task..!' value={addTodo} onChange={handelTodo} />
         <button className='bg-slate-600 px-5 rounded-lg me-2' onClick={handelAdd}>Add</button>
     </div>
   )
--- 07useContext/src/List.jsx
+++ 07useContext/src/List.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-
+import React, { useContext } from "react";
+import { TodoContext } from "./App";
 const List = () => {
+  const { saveTodo } = useContext(TodoContext);
   return (
-    <div>List</div>
-  )
-}
+    <div>
+      <h1>Todo List</h1>
+      {saveTodo.map((item,index) => {
+        return <h1 key={index}>{item}</h1>;
+      })}
+    </div>
+  );
+};
 
-export default List
+export default List;
